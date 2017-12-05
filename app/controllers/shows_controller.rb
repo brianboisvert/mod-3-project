9179c5219454e4703a52992232c0d6fece5dd6ad
Alona <github email address>
2017-12-05 09:49:54 -0500

    merging tables

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,2 +1,7 @@
 class ShowsController < ApplicationController
+  def index
+    # TODO - make dynamic with multiple show pages
+    # each at a relative path: /shows/:id
+    render json: Show.first, include: [:cast_members, :predictions]
+  end
 end
